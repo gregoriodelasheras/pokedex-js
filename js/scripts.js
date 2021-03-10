429601330f639565e9a2e83077f38221db9a927a
Francisco Gregorio de las Heras <francisco.gregoriodelasheras@gmail.com>
2021-03-10 14:34:01 +0100

    refactor: makes an array to build a Pokédex

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,122 @@
-// Const Variables
-const nameTutor = 'Itua';
+// pokemonList Array
+let pokemonList = [];
 
-// Let Variables
-let welcomeMessage = 'Hello world';
-let favoritePokemon = 'Cubone';
-let numberPokemon = null;
-
-// Displays message #1 to the user
-alert(welcomeMessage);
-
-// Displays message #2 to the user
-document.write(
-  `<h1>Hi ${nameTutor}!</h1><p>My favorite Pokémon is <strong>${favoritePokemon}</strong>!</p>`
-);
-
-// Assigns value to the 'numberPokemon' variable
-numberPokemon = 100;
-
-// Displays message #3 to the user
-document.write(
-  `<p><strong>${favoritePokemon}</strong> is the #${(numberPokemon += 4)} Pokémon.</p>`
-);
-
-// Displays message #4 to the user
-document.write(
-  `<p class="question">Do you have a favorite Pokémon, ${nameTutor}?</p>`
-);
+// Add objects to the pokemonList Array
+pokemonList = [
+  {
+    // Bulbasaur
+    // Pokémon Name
+    name: 'Bulbasaur',
+    // Pokémon Type: Primary Type, Secondary Type
+    types: ['Grass', 'Poison'],
+    // Pokémon Height
+    height: 0.7,
+    // Pokémon Weight
+    weight: 6.9,
+    // Pokémon Gender: Male, Female, Unknown
+    gender: ['Male', 'Female'],
+    // Pokémon Category
+    category: 'Seed',
+    // Pokémon Evolutions: Evolution 1, Alternative Evolution 2, Alternative Evolution 3, etc
+    evolutions: ['Ivysaur'],
+    // Pokémon Description
+    description:
+      'There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.',
+  },
+  // Ivysaur
+  {
+    name: 'Ivysaur',
+    types: ['Grass', 'Poison'],
+    height: 1,
+    weight: 13,
+    gender: ['Male', 'Female'],
+    category: 'Seed',
+    evolutions: ['Venusaur'],
+    description:
+      'When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs.',
+  },
+  // Venusaur
+  {
+    name: 'Venusaur',
+    types: ['Grass', 'Poison'],
+    height: 2,
+    weight: 100,
+    gender: ['Male', 'Female'],
+    category: 'Seed',
+    evolutions: ['None'],
+    description:
+      'Its plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight.',
+  },
+  // Charmander
+  {
+    name: 'Charmander',
+    types: ['Fire', 'None'],
+    height: 0.6,
+    weight: 8.5,
+    gender: ['Male', 'Female'],
+    category: 'Lizard',
+    evolutions: ['Charmeleon'],
+    description:
+      'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.',
+  },
+  // Charmeleon
+  {
+    name: 'Charmeleon',
+    types: ['Fire', 'None'],
+    height: 1.1,
+    weight: 19,
+    gender: ['Male', 'Female'],
+    category: 'Flame',
+    evolutions: ['Charizard'],
+    description:
+      'It has a barbaric nature. In battle, it whips its fiery tail around and slashes away with sharp claws.',
+  },
+  // Charizard
+  {
+    name: 'Charizard',
+    types: ['Fire', 'Flying'],
+    height: 1.7,
+    weight: 90.5,
+    gender: ['Male', 'Female'],
+    category: 'Flame',
+    evolutions: ['None'],
+    description:
+      'It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames.',
+  },
+  // Squirtle
+  {
+    name: 'Squirtle',
+    types: ['Water', 'None'],
+    height: 0.5,
+    weight: 9,
+    gender: ['Male', 'Female'],
+    category: 'Tiny Turtle',
+    evolutions: ['Wartortle'],
+    description:
+      'When it retracts its long neck into its shell, it squirts out water with vigorous force.',
+  },
+  // Wartortle
+  {
+    name: 'Wartortle',
+    types: ['Water', 'None'],
+    height: 1,
+    weight: 22.5,
+    gender: ['Male', 'Female'],
+    category: 'Turtle',
+    evolutions: ['Blastoise'],
+    description:
+      'It is recognized as a symbol of longevity. If its shell has algae on it, that Wartortle is very old.',
+  },
+  // Blastoise
+  {
+    name: 'Blastoise',
+    types: ['Water', 'None'],
+    height: 1.6,
+    weight: 85.5,
+    gender: ['Male', 'Female'],
+    category: 'Shellfish',
+    evolutions: ['None'],
+    description:
+      'It crushes its foe under its heavy body to cause fainting. In a pinch, it will withdraw inside its shell.',
+  },
+];
