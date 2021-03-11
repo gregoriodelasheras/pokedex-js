068dc394bb499b03fb84b18984ce8bbb51799adf
Francisco Gregorio de las Heras <francisco.gregoriodelasheras@gmail.com>
2021-03-11 16:14:29 +0100

    feat: rename array, add printPokedex function
    - Rename array from "pokemonList " to "pokedex".
    - The function printPokedex is built, to display a list of the Pokédex and indicate when a Pokémon is small, average or big size.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-// pokemonList Array
-let pokemonList = [];
+// pokedex Array
+let pokedex = [];
 
-// Add objects to the pokemonList Array
-pokemonList = [
+// Add objects to the pokedex Array
+pokedex = [
   {
     // Bulbasaur
     // Pokémon Name
@@ -120,3 +120,76 @@ pokemonList = [
       'It crushes its foe under its heavy body to cause fainting. In a pinch, it will withdraw inside its shell.',
   },
 ];
+
+// Function sizePokemon: compare sizes between pokemon
+function printPokedex() {
+  for (let i = 0; i < pokedex.length; i++) {
+    // if-conditional to indicate the number of the Pokémon in the Pokédex
+    // If it is less than 10, print before number "00"
+    if (i < 9) {
+      document.getElementById(
+        'show-pokedex'
+      ).innerHTML += `<strong>Number:</strong> 00${1 + i}<br />`;
+      // If it is greater than or equal to 10 and less than 100, print before number "0"
+    } else if (i >= 9 && i < 99) {
+      document.getElementById(
+        'show-pokedex'
+      ).innerHTML += `<strong>Number:</strong> 0${1 + i}<br />`;
+      // If it is less than 100, print only the number
+    } else {
+      document.getElementById(
+        'show-pokedex'
+      ).innerHTML += `<strong>Number:</strong> ${1 + i}<br />`;
+    }
+    // Print the name of the Pokémon
+    document.getElementById(
+      'show-pokedex'
+    ).innerHTML += `<strong>Name:</strong> ${pokedex[i].name}<br />`;
+    // Print the types of the Pokémon
+    document.getElementById(
+      'show-pokedex'
+    ).innerHTML += `<strong>Type 1:</strong> ${pokedex[i].types[0]} | <strong>Type 2:</strong> ${pokedex[i].types[1]}<br />`;
+    // Print the height of the Pokémon
+    document.getElementById(
+      'show-pokedex'
+    ).innerHTML += `<strong>Height:</strong> ${pokedex[i].height} m <br />`;
+    // Print the weight of the Pokémon
+    document.getElementById(
+      'show-pokedex'
+    ).innerHTML += `<strong>Weight:</strong> ${pokedex[i].weight} kg <br />`;
+    // Print the possible genders of the Pokémon
+    document.getElementById(
+      'show-pokedex'
+    ).innerHTML += `<strong>Genders:</strong> ${pokedex[i].gender[0]} / ${pokedex[i].gender[1]} <br />`;
+    // Print the category of the Pokémon
+    document.getElementById(
+      'show-pokedex'
+    ).innerHTML += `<strong>Category:</strong> ${pokedex[i].category} <br />`;
+    // Print the possible evolutions of the Pokémon
+    document.getElementById(
+      'show-pokedex'
+    ).innerHTML += `<strong>Evolutions:</strong> ${pokedex[i].evolutions[0]} <br />`;
+    // Print the description of the Pokémon
+    document.getElementById(
+      'show-pokedex'
+    ).innerHTML += `<strong>Description:</strong> ${pokedex[i].description} <br />`;
+    // if-conditional to measure the size of the Pokémon
+    // If it is greater than 1.6 m
+    if (pokedex[i].height > 1.6) {
+      document.getElementById('show-pokedex').innerHTML +=
+        '<strong>Wow, it’s a big Pokémon!</strong><br />';
+      // If it is between 1 m and 1.6 m
+    } else if (pokedex[i].height > 1 && pokedex[i].height < 1.6) {
+      document.getElementById('show-pokedex').innerHTML +=
+        '<strong>It’s an average Pokémon.</strong><br />';
+      // If it is less than 1 m
+    } else {
+      document.getElementById('show-pokedex').innerHTML +=
+        '<strong>It’s a tiny Pokémon!</strong><br />';
+    }
+    // Add a line break to separate each Pokémon
+    document.getElementById('show-pokedex').innerHTML += '<br />';
+  }
+}
+// Run printPokedex function
+printPokedex();
