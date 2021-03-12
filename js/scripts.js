2ad1a4aeff9cfbdbc12cac3aa4b2de24e1f9068f
Francisco Gregorio de las Heras <francisco.gregoriodelasheras@gmail.com>
2021-03-12 21:51:08 +0100

    feat: see description
    Feat:
    - Change the for loop to the forEach() function to iterate over the Pokémon in the pokemonList array to print the details of each more efficiently.
    - Create a new pokemonRepository variable in an IIFE with return data. The pokemonList array has been moved inside.
    - Adds a search filter by Pokémon name
    - Adds the option to add new Pokémon (incl. data validation)
    - Adds the option to remove the last Pokémon from the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,195 +1,221 @@
-// pokedex Array
-let pokedex = [];
+let pokemonRepository = (function () {
+  let pokemonList = [];
+  pokemonList = [
+    {
+      name: 'Bulbasaur',
+      types: ['Grass', 'Poison'],
+      height: 0.7,
+      weight: 6.9,
+      gender: ['Male', 'Female'],
+      category: 'Seed',
+      evolutions: ['Ivysaur'],
+      description:
+        'There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.',
+    },
+    {
+      name: 'Ivysaur',
+      types: ['Grass', 'Poison'],
+      height: 1,
+      weight: 13,
+      gender: ['Male', 'Female'],
+      category: 'Seed',
+      evolutions: ['Venusaur'],
+      description:
+        'When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs.',
+    },
+    {
+      name: 'Venusaur',
+      types: ['Grass', 'Poison'],
+      height: 2,
+      weight: 100,
+      gender: ['Male', 'Female'],
+      category: 'Seed',
+      evolutions: ['None'],
+      description:
+        'Its plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight.',
+    },
+    {
+      name: 'Charmander',
+      types: ['Fire', 'None'],
+      height: 0.6,
+      weight: 8.5,
+      gender: ['Male', 'Female'],
+      category: 'Lizard',
+      evolutions: ['Charmeleon'],
+      description:
+        'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.',
+    },
+    {
+      name: 'Charmeleon',
+      types: ['Fire', 'None'],
+      height: 1.1,
+      weight: 19,
+      gender: ['Male', 'Female'],
+      category: 'Flame',
+      evolutions: ['Charizard'],
+      description:
+        'It has a barbaric nature. In battle, it whips its fiery tail around and slashes away with sharp claws.',
+    },
+    {
+      name: 'Charizard',
+      types: ['Fire', 'Flying'],
+      height: 1.7,
+      weight: 90.5,
+      gender: ['Male', 'Female'],
+      category: 'Flame',
+      evolutions: ['None'],
+      description:
+        'It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames.',
+    },
+    {
+      name: 'Squirtle',
+      types: ['Water', 'None'],
+      height: 0.5,
+      weight: 9,
+      gender: ['Male', 'Female'],
+      category: 'Tiny Turtle',
+      evolutions: ['Wartortle'],
+      description:
+        'When it retracts its long neck into its shell, it squirts out water with vigorous force.',
+    },
+    {
+      name: 'Wartortle',
+      types: ['Water', 'None'],
+      height: 1,
+      weight: 22.5,
+      gender: ['Male', 'Female'],
+      category: 'Turtle',
+      evolutions: ['Blastoise'],
+      description:
+        'It is recognized as a symbol of longevity. If its shell has algae on it, that Wartortle is very old.',
+    },
+    {
+      name: 'Blastoise',
+      types: ['Water', 'None'],
+      height: 1.6,
+      weight: 85.5,
+      gender: ['Male', 'Female'],
+      category: 'Shellfish',
+      evolutions: ['None'],
+      description:
+        'It crushes its foe under its heavy body to cause fainting. In a pinch, it will withdraw inside its shell.',
+    },
+  ];
 
-// Add objects to the pokedex Array
-pokedex = [
-  {
-    // Bulbasaur
-    // Pokémon Name
-    name: 'Bulbasaur',
-    // Pokémon Type: Primary Type, Secondary Type
-    types: ['Grass', 'Poison'],
-    // Pokémon Height
-    height: 0.7,
-    // Pokémon Weight
-    weight: 6.9,
-    // Pokémon Gender: Male, Female, Unknown
-    gender: ['Male', 'Female'],
-    // Pokémon Category
-    category: 'Seed',
-    // Pokémon Evolutions: Evolution 1, Alternative Evolution 2, Alternative Evolution 3, etc
-    evolutions: ['Ivysaur'],
-    // Pokémon Description
-    description:
-      'There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.',
-  },
-  // Ivysaur
-  {
-    name: 'Ivysaur',
-    types: ['Grass', 'Poison'],
-    height: 1,
-    weight: 13,
-    gender: ['Male', 'Female'],
-    category: 'Seed',
-    evolutions: ['Venusaur'],
-    description:
-      'When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs.',
-  },
-  // Venusaur
-  {
-    name: 'Venusaur',
-    types: ['Grass', 'Poison'],
-    height: 2,
-    weight: 100,
-    gender: ['Male', 'Female'],
-    category: 'Seed',
-    evolutions: ['None'],
-    description:
-      'Its plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight.',
-  },
-  // Charmander
-  {
-    name: 'Charmander',
-    types: ['Fire', 'None'],
-    height: 0.6,
-    weight: 8.5,
-    gender: ['Male', 'Female'],
-    category: 'Lizard',
-    evolutions: ['Charmeleon'],
-    description:
-      'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.',
-  },
-  // Charmeleon
-  {
-    name: 'Charmeleon',
-    types: ['Fire', 'None'],
-    height: 1.1,
-    weight: 19,
-    gender: ['Male', 'Female'],
-    category: 'Flame',
-    evolutions: ['Charizard'],
-    description:
-      'It has a barbaric nature. In battle, it whips its fiery tail around and slashes away with sharp claws.',
-  },
-  // Charizard
-  {
-    name: 'Charizard',
-    types: ['Fire', 'Flying'],
-    height: 1.7,
-    weight: 90.5,
-    gender: ['Male', 'Female'],
-    category: 'Flame',
-    evolutions: ['None'],
-    description:
-      'It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames.',
-  },
-  // Squirtle
-  {
-    name: 'Squirtle',
-    types: ['Water', 'None'],
-    height: 0.5,
-    weight: 9,
-    gender: ['Male', 'Female'],
-    category: 'Tiny Turtle',
-    evolutions: ['Wartortle'],
-    description:
-      'When it retracts its long neck into its shell, it squirts out water with vigorous force.',
-  },
-  // Wartortle
-  {
-    name: 'Wartortle',
-    types: ['Water', 'None'],
-    height: 1,
-    weight: 22.5,
-    gender: ['Male', 'Female'],
-    category: 'Turtle',
-    evolutions: ['Blastoise'],
-    description:
-      'It is recognized as a symbol of longevity. If its shell has algae on it, that Wartortle is very old.',
-  },
-  // Blastoise
-  {
-    name: 'Blastoise',
-    types: ['Water', 'None'],
-    height: 1.6,
-    weight: 85.5,
-    gender: ['Male', 'Female'],
-    category: 'Shellfish',
-    evolutions: ['None'],
-    description:
-      'It crushes its foe under its heavy body to cause fainting. In a pinch, it will withdraw inside its shell.',
-  },
-];
-
-// Function sizePokemon: compare sizes between pokemon
-function printPokedex() {
-  for (let i = 0; i < pokedex.length; i++) {
-    // if-conditional to indicate the number of the Pokémon in the Pokédex
-    // If it is less than 10, print before number "00"
-    if (i < 9) {
-      document.getElementById(
-        'show-pokedex'
-      ).innerHTML += `<strong>Number:</strong> 00${1 + i}<br />`;
-      // If it is greater than or equal to 10 and less than 100, print before number "0"
-    } else if (i >= 9 && i < 99) {
-      document.getElementById(
-        'show-pokedex'
-      ).innerHTML += `<strong>Number:</strong> 0${1 + i}<br />`;
-      // If it is less than 100, print only the number
-    } else {
-      document.getElementById(
-        'show-pokedex'
-      ).innerHTML += `<strong>Number:</strong> ${1 + i}<br />`;
-    }
-    // Print the name of the Pokémon
-    document.getElementById(
-      'show-pokedex'
-    ).innerHTML += `<strong>Name:</strong> ${pokedex[i].name}<br />`;
-    // Print the types of the Pokémon
-    document.getElementById(
-      'show-pokedex'
-    ).innerHTML += `<strong>Type 1:</strong> ${pokedex[i].types[0]} | <strong>Type 2:</strong> ${pokedex[i].types[1]}<br />`;
-    // Print the height of the Pokémon
-    document.getElementById(
-      'show-pokedex'
-    ).innerHTML += `<strong>Height:</strong> ${pokedex[i].height} m <br />`;
-    // Print the weight of the Pokémon
-    document.getElementById(
-      'show-pokedex'
-    ).innerHTML += `<strong>Weight:</strong> ${pokedex[i].weight} kg <br />`;
-    // Print the possible genders of the Pokémon
-    document.getElementById(
-      'show-pokedex'
-    ).innerHTML += `<strong>Genders:</strong> ${pokedex[i].gender[0]} / ${pokedex[i].gender[1]} <br />`;
-    // Print the category of the Pokémon
-    document.getElementById(
-      'show-pokedex'
-    ).innerHTML += `<strong>Category:</strong> ${pokedex[i].category} <br />`;
-    // Print the possible evolutions of the Pokémon
-    document.getElementById(
-      'show-pokedex'
-    ).innerHTML += `<strong>Evolutions:</strong> ${pokedex[i].evolutions[0]} <br />`;
-    // Print the description of the Pokémon
-    document.getElementById(
-      'show-pokedex'
-    ).innerHTML += `<strong>Description:</strong> ${pokedex[i].description} <br />`;
-    // if-conditional to measure the size of the Pokémon
-    // If it is greater than 1.6 m
-    if (pokedex[i].height > 1.6) {
-      document.getElementById('show-pokedex').innerHTML +=
-        '<strong>Wow, it’s a big Pokémon!</strong><br />';
-      // If it is between 1 m and 1.6 m
-    } else if (pokedex[i].height > 1 && pokedex[i].height < 1.6) {
-      document.getElementById('show-pokedex').innerHTML +=
-        '<strong>It’s an average Pokémon.</strong><br />';
-      // If it is less than 1 m
-    } else {
-      document.getElementById('show-pokedex').innerHTML +=
-        '<strong>It’s a tiny Pokémon!</strong><br />';
-    }
-    // Add a line break to separate each Pokémon
-    document.getElementById('show-pokedex').innerHTML += '<br />';
+  function getAll() {
+    return pokemonList;
   }
+
+  // Bonus Task 1 and 2
+  function add(item) {
+    if (typeof item === 'object') {
+      console.log(
+        'Object has all the keys? - ' +
+          Object.keys(pokemonList[0]).every((key) => key in item)
+      );
+      if (Object.keys(pokemonList[0]).every((key) => key in item)) {
+        alert(
+          `You have discovered a new Pokémon! "${item.name}" data has been entered into the Pokédex.`
+        );
+        pokemonList.push(item);
+      } else {
+        alert(
+          `The data for the new Pokémon you are trying to add is not complete. Please verify that no fields are missing.`
+        );
+      }
+    } else {
+      alert(
+        `"${item}" is not a valid Pokémon! Please check that the data type typed in is an object.`
+      );
+    }
+  }
+
+  function remove(item) {
+    pokemonList.pop(item);
+    alert('The last Pokémon has been deleted');
+  }
+
+  // Bonus Task 3: find specific Pokémon only by name
+  let button = document.getElementById('filter');
+  button.onclick = function () {
+    let filterPkm = pokemonRepository.getAll().filter(function (pkm) {
+      return pkm.name === document.getElementById('searchPkm').value;
+    });
+    console.log(filterPkm);
+  };
+
+  return {
+    getAll: getAll,
+    add: add,
+    remove: remove,
+  };
+})();
+
+// Bonus Task 1 & 2: Add a valid object 1
+pokemonRepository.add({
+  name: 'Panchomon',
+  types: ['Rock', 'Roll'],
+  height: 1.7,
+  weight: 80,
+  gender: ['Male', 'Latin Lover'],
+  category: 'WebDev?',
+  evolutions: ['PanchoDev'],
+  description:
+    'He studies and programs a lot in order to become a good web developer.',
+});
+
+// Bonus Task 1 & 2: Add a valid object 2 (to remove)
+pokemonRepository.add({
+  name: 'PanchoDev',
+  types: ['Rock', 'Roll'],
+  height: 1.65,
+  weight: 200,
+  gender: ['Male', 'Super Macho'],
+  category: 'WebDev',
+  evolutions: ['None'],
+  description:
+    'Following the wise advice of his tutor Itua and mentor Vinh Tuong, he has managed to achieve mastery of web programming.',
+});
+
+// Bonus Task 1 & 2: Add a invalid object 1 (missing data)
+pokemonRepository.add({
+  name: 'Bla',
+  types: ['Bla', 'Bla'],
+  height: 1,
+  weight: 800,
+  gender: ['Bla1', 'Bla2'],
+  category: 'Bla',
+  evolutions: ['BlaBla'],
+});
+
+// Bonus Task 1 & 2: Add an invalid object 2
+pokemonRepository.add('Digimon');
+
+// Bonus Task: Remove last valid object
+pokemonRepository.remove();
+
+// Print Pokédex
+function printPokedex(pkm) {
+  let sizePkm =
+    pkm.height > 1.6
+      ? 'Wow, it’s a big Pokémon!'
+      : pkm.height > 1 && pkm.height < 1.6
+      ? 'It’s an average Pokémon.'
+      : 'It’s a tiny Pokémon!';
+
+  // Print Pokédex
+  document.getElementById('show-pokedex').innerHTML += `
+    <div class="pokecard">
+      <p><strong>Name:        </strong>${pkm.name}           </p>
+      <p><strong>Type 1:      </strong>${pkm.types[0]}       | <strong>Type 2:</strong> ${pkm.types[1]}</p>
+      <p><strong>Height:      </strong>${pkm.height}         m</p>
+      <p><strong>Weight:      </strong>${pkm.weight}         kg</p>
+      <p><strong>Genders:     </strong>${pkm.gender[0]}      / ${pkm.gender[1]}</p>
+      <p><strong>Category:    </strong>${pkm.category}       </p>
+      <p><strong>Evolutions:  </strong>${pkm.evolutions[0]}  </p>
+      <p><strong>Description: </strong>${pkm.description}    </p>
+      <p><strong>${sizePkm}</p>
+    </div>`;
 }
-// Run printPokedex function
-printPokedex();
+
+pokemonRepository.getAll().forEach(printPokedex);
